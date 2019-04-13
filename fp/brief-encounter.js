9cafe2804912538e0b179187c5675992027b6bf8
AndriiSiuta <andriisiuta@gmail.com>
2019-04-13 14:16:56 +0300

    feat: Add fp to my self. Write simple first class functions

---
+++ fp/brief-encounter.js
@@ -0,0 +1,9 @@
+const addTwo = (a, b) => a + b;
+const multiPlyTwo = (a, b) => a * b;
+
+const a1 = 2;
+const a2 = 3;
+const a3 = 6;
+
+const result = addTwo(multiPlyTwo(a1, a2), a3);
+console.log('r', result); // 12
